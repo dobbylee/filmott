113b8277a0fcf7ac531a706f721f26c9fa24e794
dobbylee <leekw1245@gmail.com>
2026-02-24 12:12:19 +0900

    refactor: extend CreateDto with PartialType for UpdateDto

--- backend/src/posts/dto/update-post.dto.ts
+++ backend/src/posts/dto/update-post.dto.ts
@@ -1,13 +1,4 @@
-import { IsOptional, IsString, MinLength } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePostDto } from './create-post.dto';
 
-export class UpdatePostDto {
-  @IsOptional()
-  @IsString()
-  @MinLength(2, { message: 'Title must be at least 2 characters long.' })
-  title?: string;
-
-  @IsOptional()
-  @IsString()
-  @MinLength(10, { message: 'Content must be at least 10 characters long.' })
-  content?: string;
-}
+export class UpdatePostDto extends PartialType(CreatePostDto) {}
